df37df83279c7b891698e47cc8da54f94d1a7a38
peterdongan <peterdongan@gmail.com>
2020-05-28 10:12:48 +0100

    file header comments

--- UndoService/UndoService/StateSetEventArgs.cs
+++ UndoService/UndoService/StateSetEventArgs.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Peter Dongan. All rights reserved.
+// Licensed under the MIT licence. https://opensource.org/licenses/MIT
+// Project: https://github.com/peterdongan/UndoService
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
